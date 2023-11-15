309ba02093313926a951c78fe68a93c670fcccc5
DuXisXl <eduardoramos245r@gmail.com>
2023-11-15 17:03:34 -0300

    função de verificação de dados

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 (async () => {
     const Planet = require("./models/Planet");
-  
+/*  Modo para inserção de dados(Planetas)
     const newPlanet = await Planet.create({
       name: "Saturno",
       position: 8,
     });
     console.log(newPlanet);
+*/
+
+const seePlanets = await Planet.findAll({
+  where: {
+    name: "Terra",
+  },
+});
+console.log(seePlanets);
   })();
